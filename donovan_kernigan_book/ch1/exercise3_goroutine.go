42004bf16234843c28381c09fd6b6f7a808d3bf5
adrianoff <adrianov87@gmail.com>
2022-01-14 19:13:59 +0300

    still learning

--- donovan_kernigan_book/ch1/exercise3_goroutine.go
+++ donovan_kernigan_book/ch1/exercise3_goroutine.go
@@ -1,5 +1,42 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	urls := [2]string{"https://facebook.com", "https://google.com"}
+
+	for _, url := range urls {
+		go fetch(url, ch)
+	}
+	for range urls {
+		fmt.Println(<-ch) // receive from channel ch
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	//start := time.Now()
+	_, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send to channel ch
+		return
+	}
+	ch <- url + " DONE"
+	//_, err := httputil.DumpResponse(resp, true)
+	//fmt.Println(string(b))
+
+	//nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	//resp.Body.Close() // don't leak resources
+	//if err != nil {
+	//	ch <- fmt.Sprintf("while reading %s: %v", url, err)
+	//	return
+	//}
+	//secs := time.Since(start).Seconds()
+	//ch <- fmt.Sprintf("%.2fs  %7d  %s", secs, nbytes, url)
 }
