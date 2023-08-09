79403f7134d355ac78c8bb36054c270426907722
adrianoff <adrianov87@gmail.com>
2023-08-09 22:14:14 +0300

    go learning

--- yandex_practicum/exercises/structures.go
+++ yandex_practicum/exercises/structures.go
@@ -14,14 +14,39 @@ type Person struct {
 
 type Response struct {
 	Header struct {
-		Code: int,
-		Message: string
-	}
+		Code    int    `json:"code"`
+		Message string `json:"message"`
+	} `json:"header"`
+	Data []struct {
+		Type       string `json:"type"`
+		ID         int    `json:"id"`
+		Attributes struct {
+			Email      string `json:"email"`
+			ArticleIds []int  `json:"article_ids"`
+		} `json:"attributes"`
+	} `json:"data"`
 }
+
+const rawResp = `
+{
+    "header": {
+        "code": 0,
+        "message": ""
+    },
+    "data": [{
+        "type": "user",
+        "id": 100,
+        "attributes": {
+            "email": "bob@yandex.ru",
+            "article_ids": [10, 11, 12]
+        }
+    }]
+}
+`
 
 func main() {
 	fmt.Println("Hi")
-	test1()
+	test2()
 }
 
 func test1() {
@@ -30,4 +55,15 @@ func test1() {
 	str, _ := json.Marshal(p)
 
 	fmt.Println(string(str))
+}
+
+
+func test2() {
+	resp := Response{}
+		
+	if err := json.Unmarshal([]byte(rawResp), &resp); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%+v\n", resp)
 }
