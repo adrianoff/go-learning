020eb8b6eeec4f03499a5edfba7884b3f019a696
adrianoff <adrianov87@gmail.com>
2023-09-24 23:21:30 +0300

    import

---
+++ yandex_practicum/exercises/packages_exercise/exercise/mathslices.go
@@ -0,0 +1,13 @@
+package mathslices
+
+func SumSlice(s []int) int {
+	return 0
+}
+
+func MapSlice(s []int, op func(int) int) {
+	return
+}
+
+func FoldSlice(s []int, op func(int, int) int, init int) int {
+	return 0
+}
---
+++ yandex_practicum/exercises/packages_exercise/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "exercise/mathslices"
+
+func main() {
+	mathslices.SumSlice(make([]int, 0))
+}
